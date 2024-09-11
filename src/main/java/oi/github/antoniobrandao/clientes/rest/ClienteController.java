d5df56777e301f49e87be0e1a5b9f45307874e42
Pedro Augusto B. Gondim <124090320+PedroAugusto10500@users.noreply.github.com>
2024-09-11 09:11:32 -0300

    Update ClienteController.java

--- src/main/java/oi/github/antoniobrandao/clientes/rest/ClienteController.java
+++ src/main/java/oi/github/antoniobrandao/clientes/rest/ClienteController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
-@CrossOrigin(origins = {"*"},
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Configuration
